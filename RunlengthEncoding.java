db81b9a5cfeb950dc1ea81170596cca39666cf24
Kiệt Tuấn <tuankietng61@gmail.com>
2025-04-28 10:36:30 +0700

    Add files via upload

---
+++ RunlengthEncoding.java
@@ -0,0 +1,53 @@
+package hus.oop.rle;
+
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class RunlengthEncoding {
+    public static void main(String[] args) {
+        /*
+        TODO:
+
+        - Viết các hàm test mã hóa và giải mã theo mã hóa run-length và in ra theo mẫu:
+        Ví dụ:
+        Decoded Text: xxxxx
+        Encoded Text: xxxxx
+
+        và
+
+        Encoded Text: xxxxx
+        Decoded Text: xxxxx
+
+
+         */
+    }
+
+    /*
+     * Hàm mã hóa chuỗi ký tự text theo mã hóa run-length.
+     */
+    public static String encoding(String text) {
+        /* TODO */
+    }
+
+    /*
+     * Hàm giải mã chuỗi ký tự text theo mã hóa run-length.
+     */
+    public static String decoding(String text) {
+        /* TODO */
+    }
+
+    /*
+     * Hàm test mã hóa theo mã hóa run-length.
+     */
+    public static void testEncoding() {
+        /* TODO */
+    }
+
+    /*
+     * Hàm test giải mã theo mã hóa run-length.
+     */
+    public static void testDecoding() {
+        /* TODO */
+    }
+}
