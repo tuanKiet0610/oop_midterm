9daaa534c699e452e55b2412baff1f280fe4fa3d
Kiệt Tuấn <tuankietng61@gmail.com>
2025-04-28 10:38:34 +0700

    Add files via upload

--- RunlengthEncoding.java
+++ RunlengthEncoding.java
@@ -1,18 +1,25 @@
-package hus.oop.rle;
+package deki2_2425_de1.rle;
 
 import java.util.LinkedHashSet;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Set;
 
 public class RunlengthEncoding {
     public static void main(String[] args) {
+        testEncoding();
+        testDecoding();
+
         /*
         TODO:
+
 
         - Viết các hàm test mã hóa và giải mã theo mã hóa run-length và in ra theo mẫu:
         Ví dụ:
         Decoded Text: xxxxx
         Encoded Text: xxxxx
+
+
 
         và
 
@@ -28,6 +35,30 @@ public class RunlengthEncoding {
      */
     public static String encoding(String text) {
         /* TODO */
+        StringBuffer sb = new StringBuffer();
+        int count;
+        int i = 0;
+        while (i < text.length()) {
+            char c = text.charAt(i);
+            count = 1;
+            for (int j = i + 1; j < text.length(); j++) {
+                char c2 = text.charAt(j);
+                if (c == c2) {
+                    count++;
+                    i++;
+                }else {
+                    sb.append(count).append(c);
+                    i++;
+                    break;
+                }
+            }
+            if(i == text.length() - 1){
+                sb.append(count).append(c);
+                break;
+            }
+        }
+        return sb.toString();
+
     }
 
     /*
@@ -35,6 +66,15 @@ public class RunlengthEncoding {
      */
     public static String decoding(String text) {
         /* TODO */
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < text.length(); i = i + 2) {
+            char c = text.charAt(i);
+            char c2 = text.charAt(i + 1);
+            for (int j = 0; j < c - '0'; j++) {
+                sb.append(c2);
+            }
+        }
+        return sb.toString();
     }
 
     /*
@@ -42,6 +82,10 @@ public class RunlengthEncoding {
      */
     public static void testEncoding() {
         /* TODO */
+        String decodedtext = "aaaaabcccaa";
+        System.out.print("Decoded Text: " + decodedtext);
+        System.out.println();
+        System.out.println("Encoded Text: " + encoding(decodedtext));
     }
 
     /*
@@ -49,5 +93,10 @@ public class RunlengthEncoding {
      */
     public static void testDecoding() {
         /* TODO */
+        String encodedtext = "3e4f2e";
+        System.out.print("Encoded Text: " + encodedtext);
+        System.out.println();
+        System.out.println("Decoded Text: " + decoding(encodedtext));
     }
 }
+
